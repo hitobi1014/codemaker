93fe57b5d4d6601d39809a19501cea50511db823
dspark <fmfhfmfh@gmail.com>
2020-11-30 18:46:39 +0900

    xml 수정중

---
+++ src/test/java/kr/co/codemaker/admin/notice/dao/NoticeMapperTest.java
@@ -0,0 +1,14 @@
+package kr.co.codemaker.admin.notice.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NoticeMapperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
