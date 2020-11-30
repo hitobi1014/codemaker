77bb6cb76d4aa4724a09188ed47409c9c52756f8
dy <qwedmswl@gmail.com>
2020-11-30 16:55:04 +0900

    admin/login

--- src/main/java/kr/co/codemaker/common/controller/AdminController.java
+++ src/main/java/kr/co/codemaker/common/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import kr.co.codemaker.admin.vo.AdminVO;
 import kr.co.codemaker.common.service.AdminService;
 
 @Controller
@@ -18,39 +19,39 @@ import kr.co.codemaker.common.service.AdminService;
 public class AdminController { 
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 	
-	@Resource(name="adminService")
-	private AdminService adminService;
-	
-	@RequestMapping(path="/login", method = RequestMethod.GET)
-	public String getView() {
-		logger.debug("AdminController.getView()");
-		return "admin/login/adminLogin";
-	}
-	/**
-	 * 로그인 화면
-	 * @since 2020. 11. 30
-	 * @author 이은지
-	 * @return
-	 */
-	
-	@RequestMapping(path="/login", method=RequestMethod.POST)
-	public String login(AdminVO adminVo, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
-		logger.info("post login");
-		
-		HttpSession session = req.getSession();
-		AdminVO login = adminService.getAdmin(adminVo);
-		
-		if(login == null) {
-			session.setAttribute("admin", null);
-			rttr.addFlashAttribute("msg", false);
-		}
-		else {
-			session.setAttribute("admin", login);
-		}
-		logger.debug("login : {}", login);
-		
-		return "admin/login/adminLogin";
-	}
+//	@Resource(name="adminService")
+//	private AdminService adminService;
+//	
+//	@RequestMapping(path="/login", method = RequestMethod.GET)
+//	public String getView() {
+//		logger.debug("AdminController.getView()");
+//		return "admin/login/adminLogin";
+//	}
+//	/**
+//	 * 로그인 화면
+//	 * @since 2020. 11. 30
+//	 * @author 이은지
+//	 * @return
+//	 */
+//	
+//	@RequestMapping(path="/login", method=RequestMethod.POST)
+//	public String login(AdminVO adminVo, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
+//		logger.info("post login");
+//		
+//		HttpSession session = req.getSession();
+//		AdminVO login = adminService.getAdmin(adminVo);
+//		
+//		if(login == null) {
+//			session.setAttribute("admin", null);
+//			rttr.addFlashAttribute("msg", false);
+//		}
+//		else {
+//			session.setAttribute("admin", login);
+//		}
+//		logger.debug("login : {}", login);
+//		
+//		return "admin/login/adminLogin";
+//	}
 	
 	
 	@RequestMapping(path="/adminMain")
