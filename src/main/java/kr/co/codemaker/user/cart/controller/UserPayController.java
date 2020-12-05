3fd557198be6a5431b18c25026fb459cb223b49a
DESKTOP-MINJUN\hitob <hitobi1014@gmail.com>
2020-12-05 17:53:16 +0900

    회원결제하는중

--- src/main/java/kr/co/codemaker/user/cart/controller/UserPayController.java
+++ src/main/java/kr/co/codemaker/user/cart/controller/UserPayController.java
@@ -1,19 +1,28 @@
 package kr.co.codemaker.user.cart.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.co.codemaker.common.vo.UserVO;
-import kr.co.codemaker.user.cart.dao.UserPayMapper;
 import kr.co.codemaker.user.cart.service.UserPayService;
 import kr.co.codemaker.user.cart.vo.CartVO;
 import kr.co.codemaker.user.cart.vo.LessonVO;
@@ -31,18 +40,29 @@ public class UserPayController {
 	private UserPayService userPayService;
 	
 	@RequestMapping(path="user/payView")
-	public String payView(HttpSession session, Model model, PayVO payVo, LessonVO lessonVo) {
+	public String payView(HttpSession session, Model model, PayVO payVo, LessonVO lessonVo, @RequestParam(name="lessonVo[]")List<LessonVO> lessonVoList) {
+//		ObjectMapper jsonMapper = new ObjectMapper();
+//		LessonVO lvo =new LessonVO();
+//		try {
+//			lvo = jsonMapper.readValue(jsonData, LessonVO.class);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		logger.debug("데이터 : {}", lessonVoList.get(0).getLesNm());
+		
+//		logger.debug("강의 정보 :{}",lessonVo);
+		return "";
 		// 회원아이디, 강의 아이디 필요함
-		UserVO userVo = (UserVO) session.getAttribute("MEMBER_INFO");	//로그인한 회원아이디 세션에서 가져오기
-		LessonVO getLessonVo = null;
-		try {
-			getLessonVo = userPayService.selectLessonInfo(lessonVo);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		model.addAttribute("userVo", userVo);
-		model.addAttribute("lessonVo", getLessonVo);
-		return "mainT/user/payment/pay";
+//		UserVO userVo = (UserVO) session.getAttribute("MEMBER_INFO");	//로그인한 회원아이디 세션에서 가져오기
+//		LessonVO getLessonVo = null;
+//		try {
+//			getLessonVo = userPayService.selectLessonInfo(lessonVo);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		model.addAttribute("userVo", userVo);
+//		model.addAttribute("lessonVo", getLessonVo);
+//		return "mainT/user/payment/pay";
 	}
 	
 	//결제하기
@@ -93,7 +113,7 @@ public class UserPayController {
 	
 	//장바구니로 이동, 담은강의 리스트 조회
 	@RequestMapping(path="user/cartView")
-	public String cartView(HttpSession session) {
+	public String cartView(HttpSession session,Model model,LessonVO lessonVo) {
 		UserVO userVo = (UserVO) session.getAttribute("MEMBER_INFO");
 		List<CartVO> cartList = null;
 		try {
@@ -114,6 +134,7 @@ public class UserPayController {
 			lessonList.add(lesson);
 		}
 		logger.debug("추가된 강의 정보 :{}",lessonList);
+		model.addAttribute("lessonList", lessonList);
 		
 		return "mainT/user/payment/cart";
 	}
--- src/main/webapp/WEB-INF/views/user/payment/cart.jsp
+++ src/main/webapp/WEB-INF/views/user/payment/cart.jsp
@@ -1,3 +1,60 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<style>
+tr, td{
+border:1px solid black;
+}
+</style>
 <h1>장바구니 입니다</h1>
+<table>
+	<tr>
+		<th>체크박스</th>
+		<th>강의아이디</th>
+		<th>강의명</th>
+		<th>강사명</th>
+		<th>수강기간</th>
+		<th>금액</th>
+	</tr>
+<%-- 	<c:url value="/user/payView" var="payView"/> --%>
+		<c:forEach items="${lessonList}" var="lesson">
+			<tr>
+				<td><input type="checkbox" class="lesChk"/></td>
+				<td><input type="text" value="${lesson.lesId}" name="lesId" readonly="readonly"/></td>
+				<td><input type="text" value="${lesson.lesNm}" name="lesNm" readonly="readonly"/></td>
+				<td><input type="text" value="${lesson.tchNm}" name="tchNm" readonly="readonly"/></td>
+				<td><input type="text" value="${lesson.lesTerm}" name="lesTerm" readonly="readonly"/></td>
+				<td><input type="text" value="${lesson.lesCash}" name="lesCash" readonly="readonly"/></td>
+			</tr>
+		</c:forEach>
+	<button id="sub">결제하기</button>
+</table>
+<script>
+$(function(){
+	$("#sub").on('click',function(){
+		var lessonVo = [];
+		$(".lesChk:checked").each(function(){
+			var td = $(this).parent().parent().children();
+			var lvo = td.children().serialize();
+			lessonVo.push(lvo);
+		})
+// 		console.log(lessonVo);
+// 		var jsonData = JSON.stringify(lessonVo);
+// 		console.log(jsonData);
+		$.ajax({
+			url : '<c:url value="/user/payView"/>',
+			method : 'post',
+			data : {lessonVo : lessonVo},
+			success : function(res){
+				alert("성공");
+			},
+			error : function(xhr){
+				alert(xhr);
+			}
+			
+		})
+	})
+})
+
+</script>
