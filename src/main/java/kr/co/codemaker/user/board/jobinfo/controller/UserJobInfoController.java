4c8fe6293e800b94a59f4844953176eb60edb921
dam <qkrekal1389@gmail.com>
2020-12-22 18:04:44 +0900

    커밋

--- src/main/java/kr/co/codemaker/user/board/jobinfo/controller/UserJobInfoController.java
+++ src/main/java/kr/co/codemaker/user/board/jobinfo/controller/UserJobInfoController.java
@@ -93,7 +93,9 @@ public class UserJobInfoController {
 		
 		FilesVO filesVo = filesService.selectFiles(filesId);
 		
-		response.setHeader("Content-Disposition", "attachment; filename=\""+filesVo.getFilesNm()+"\"");
+		String filesNm = new String(filesVo.getFilesNm().getBytes("UTF-8"), "ISO-8859-1");
+		
+		response.setHeader("Content-Disposition", "attachment; filename=\""+filesNm+"\"");
 		response.setContentType("application/octet-stream");
 		
 		FileInputStream fis = new FileInputStream("D:\\profile\\" + filesVo.getFilesNm());
